71273f16e2625a09d3a3fc54d62f19949fcf061b
Satyam1209 <satyamsingh12092000@gmail.com>
2025-11-15 21:42:41 -0800

    project structure,exception,logger

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -9,7 +10,14 @@ def error_message_details(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_detail)
+        logging.info(str(self.error_message))
     def __str__(self):
         return self.error_message
+    
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        raise CustomException(e,sys) 
